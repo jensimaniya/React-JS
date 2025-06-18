1ff14b84aee9dc48509e7833da7718a644dd4e8f
jensimaniya <jensimaniya@gmail.com>
2025-06-18 17:06:12 +0530

    components

--- LW-4/src/App.jsx
+++ LW-4/src/App.jsx
@@ -1,16 +1,24 @@
-
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css'
-import Accordions from './components/Accordion'
+import "./App.css";
+import Accordions from "./components/Accordion";
+import "tailwindcss";
+import HeroSection from "./components/telwindCss/herosection";
+import Demo from "./components/chakrUI/chakrui";
+import { Provider } from "./components/ui/provider";
 
 function App() {
- 
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <Accordions />
+      <Accordions />
+      <HeroSection />
+      <Provider>
+        <Demo />
+      </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
