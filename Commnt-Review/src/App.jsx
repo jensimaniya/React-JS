01a8c17ec869d0c84db6f2f7146d8bc2ddf00194
jensimaniya <jensimaniya@gmail.com>
2025-07-01 16:34:17 +0530

    comment-review

---
+++ Commnt-Review/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Comment from './comment'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Comment/>  
+      
+    </>
+  )
+}
+
+export default App
