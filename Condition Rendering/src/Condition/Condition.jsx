d7c05156c7d61afbeb180de5eed31fee5f356517
jensimaniya <jensimaniya@gmail.com>
2025-07-10 15:47:30 +0530

    condition rendaring

--- Condition Rendering/src/Condition/Condition.jsx
+++ Condition Rendering/src/Condition/Condition.jsx
@@ -9,14 +9,8 @@ const Conditionui = () => {
 };
 
 export const Product = (props) => {
-  // return (
-  //     <>
-  //         <li>{props.name}</li>
-
-  //     </>
-  // )
-
-  if (props.stock < 35) {
+ 
+  if (props.stock < 20) {
     return <li style={{ color: "red", listStyle: "none" }}>{props.name}</li>;
   } else if (props.stock > 35) {
     return <li style={{ color: "green", listStyle: "none" }}>{props.name}</li>;
@@ -29,16 +23,14 @@ export const Productlist = () => {
   return (
     <>
       <ul>
-        <Product name="Refrigerator" stock={10}></Product>
-        <Product name="Laptop" stock={100}></Product>
-        <Product name="mobile" stock={30}></Product>
-        <Product name="oven" stock={50}></Product>
-        <Product name="juicer" stock={90}></Product>
-        <Product name="Tv" stock={35}></Product>
-        <Product name="ac" stock={45}></Product>
-        <Product name="culler" stock={25}></Product>
-        <Product name="waching machin" stock={35}></Product>
-        <Product name="blender" stock={65}></Product>
+        <Product name="AC" stock={10}></Product>
+        <Product name="Laptop" stock={10}></Product>
+        <Product name="Computer" stock={30}></Product>
+        <Product name="Fridge" stock={50}></Product>
+        <Product name="TV" stock={90}></Product>
+        <Product name="Mixer" stock={35}></Product>
+        <Product name="Blender" stock={45}></Product>
+        
       </ul>
     </>
   );
